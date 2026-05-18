c53b993631b293c259fd2811e332b34bbe0dcde2
diksha.bhardwaj <diksha.bhardwaj@sonartechnologies.com>
2026-05-18 11:42:39 +0530

    small changes

---
+++ DataBase/DB_Scripts/getuserbyemail.UserDefinedFunction.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION public.getuserbyemail(p_email text)
+ RETURNS TABLE(id integer, name character varying, email character varying, passwordhash text, roleid integer)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT u.id, u.name, u.email, u.passwordhash, u.roleid 
+    FROM users u 
+    WHERE LOWER(u.email) = LOWER(p_email) 
+    LIMIT 1;
+END;
+$function$
+;
