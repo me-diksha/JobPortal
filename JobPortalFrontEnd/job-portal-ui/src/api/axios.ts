a4f4726517da8925809680bba389f236692ca6f8
diksha.bhardwaj <diksha.bhardwaj@sonartechnologies.com>
2026-07-30 15:57:31 +0530

    Frontend

---
+++ JobPortalFrontEnd/job-portal-ui/src/api/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://localhost:7237/api", // change according to your .NET API URL
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+
+// Automatically attach JWT token
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+
+export default api;
