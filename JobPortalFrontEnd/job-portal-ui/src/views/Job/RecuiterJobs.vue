f6a27a04280f093f66ef825780251700df80a317
diksha.bhardwaj <diksha.bhardwaj@sonartechnologies.com>
2026-09-14 08:43:30 +0530

    Delete Modal

--- JobPortalFrontEnd/job-portal-ui/src/views/Job/RecuiterJobs.vue
+++ JobPortalFrontEnd/job-portal-ui/src/views/Job/RecuiterJobs.vue
@@ -66,6 +66,9 @@ const openStatusJobId =
 
 const statusLoading =
     ref(false);
+const ischangeDelete = ref(false);
+const selectedJob = ref<Job | null>(null);
+
 
 
 /* =========================
@@ -284,15 +287,21 @@ const editJob = (
     });
 
 };
-
-const handleDelete = async(job :Job) =>{
+const DeleteClicked = (job: Job) => {
+    selectedJob.value = job;
+    ischangeDelete.value = true;
+};
+const handleDelete = async() =>{
+    if (!selectedJob.value) return;
     try{
-         var id = job.id;
+         var id = selectedJob.value.id;
         const response =
                 await DeleteJob(id);
         if(response.status== HttpStatusCode.Ok)   {
             toast.success("Job deleted successfully");
-            loadJobs();
+            ischangeDelete.value = false;
+            selectedJob.value = null;
+           await loadJobs();
         }   
     }catch(err){
         console.error("Can't delete",err);
@@ -846,7 +855,7 @@ onMounted(() => {
                                     </button>
 
                                     <button
-                                        class="delete-btn" @click ="handleDelete(job)"
+                                        class="delete-btn" @click ="DeleteClicked(job)"
                                     >
                                         Delete
                                     </button>
@@ -866,6 +875,23 @@ onMounted(() => {
             </div>
 
         </section>
+        <div v-if="ischangeDelete" class="modal-overlay">
+            <div class="success-modal">
+                <div class="success-icon">
+                    ⚠️
+                </div>
+
+                <h2>Are You Sure You Want to Delete this Job ?</h2>
+
+
+                <button class="modal-btn" @click="handleDelete">
+                    Yes
+                </button>
+                <button class ="modal-btn" @click="ischangeDelete=false">
+                    Cancel
+                </button>
+            </div>
+        </div>
 
     </div>
 
@@ -1780,8 +1806,90 @@ onMounted(() => {
     margin-bottom: 20px;
 
 }
+/*modal */
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    z-index: 9999;
+}
+
+.success-modal {
+    background: white;
+    width: 400px;
+    padding: 35px;
+    border-radius: 12px;
+
+    text-align: center;
+
+    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.2);
+
+    animation: modalFadeIn 0.25s ease;
+}
+
+.success-modal h2 {
+    margin: 15px 0 10px;
+    color: rgb(24, 46, 107);
+}
+
+.success-modal p {
+    color: #666;
+    margin-bottom: 25px;
+}
+
+.success-icon {
+    width: 70px;
+    height: 70px;
+
+    margin: 0 auto;
+
+    border-radius: 50%;
+
+    color: white;
+
+    font-size: 42px;
+    font-weight: bold;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal-btn {
+    background-color: rgb(24, 46, 107);
+    color: white;
+    margin:10px;
+    border: none;
+    border-radius: 5px;
+
+    padding: 10px 30px;
+
+    cursor: pointer;
+    font-size: 15px;
+}
+
+.modal-btn:hover {
+    background: #334f9c;
+}
+@keyframes modalFadeIn {
+    from {
+        opacity: 0;
+        transform: scale(0.9);
+    }
+
+    to {
+        opacity: 1;
+        transform: scale(1);
+    }
+}
 
 /* =========================
    RESPONSIVE
